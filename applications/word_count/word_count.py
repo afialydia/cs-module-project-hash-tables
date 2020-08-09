9e5132292acedc5b62e37544e2975b3b307d2247
Afia-Lydia <you@example.com>
2020-08-09 14:34:23 -0400

    completed three problems

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,28 @@
 def word_count(s):
     # Your code here
 
+    cache = {}
+
+    for word in s.split():
+        word = word.lower()
+
+        punctuation = '":;,.-+=/\\|[]{}()*^&'
+        for ele in word:
+            if ele in punctuation:
+                word = word.replace(ele, "")
+
+        if word in cache:
+            cache[word] += 1
+
+        elif word != '':
+            cache.update({ word: 1 })
+
+    return cache
 
 
-if __name__ == "__main__":
-    print(word_count(""))
-    print(word_count("Hello"))
-    print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+print(word_count("let's go out!"))
+# if __name__ == "__main__":
+#     # print(word_count(""))
+#     print(word_count("Hello"))
+#     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+#     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
